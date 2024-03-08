60b59dca64eed20bce6d6d1c366ffb6854ccd707
Ondřej Staněk <stanekondrej@skiff.com>
2024-03-08 18:00:12 +0100

    initial commit

---
+++ app/src/main/java/me/ondrej/thinkaboutit/MainActivity.kt
@@ -0,0 +1,43 @@
+package me.ondrej.thinkaboutit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import me.ondrej.thinkaboutit.ui.theme.ThinkAboutItTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ThinkAboutItTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ThinkAboutItTheme {
+        Greeting("Android")
+    }
+}
